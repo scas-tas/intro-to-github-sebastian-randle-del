b3c7559218fc3684f9722c0a92ff9f9523e771ed
sebastian-randle-del <sebastian-randle@scas.nsw.edu.au>
2026-03-18 12:32:46 +1100

    C
    COFFEE

---
+++ code stuff you know.py
@@ -0,0 +1,26 @@
+"""inp = input('What did you say? ')
+copy = inp.title()
+cat = copy.upper()
+print(cat)
+
+while echo != 'I love brussel sprouts!':
+  echo = input('What did you say? ')  
+  cat = echo.upper()
+  if echo != 'I love brussel sprouts':
+    print(cat)
+print("You're no fun!")"""
+
+
+
+
+inp = input('What did you say? ')
+copy = inp.title()
+cat = copy.upper()
+print(cat)
+while copy != 'I love brussel sprouts!':
+  copy = input('What did you say? ')
+  up_man = copy.title()
+  cat = up_man.upper()
+  if copy != 'I love brussel sprouts!':
+    print(cat)
+print("You're no fun!")
