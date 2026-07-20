ebe9b6612cd9e39d4f3349740cb58e57aa8da52d
sebastian-randle-del <sebastian-randle@scas.nsw.edu.au>
2026-07-20 10:33:45 +1000

    assessment 2 all

---
+++ Assessment task 2/ciper_challenge_Q4.py
@@ -0,0 +1,6 @@
+# ciper challenge
+def encode(message: str, shift: int) -> str:
+    pass
+ 
+def decode(message: str, shift: int) -> str:
+    pass
---
+++ Assessment task 2/inventory_manager_Q3.py
@@ -0,0 +1,10 @@
+# inventory manager
+def add_item(inventory: dict, name: str, quantity: int) -> None:
+    pass
+ 
+def remove_item(inventory: dict, name: str, quantity: int) -> None:
+    pass
+ 
+def get_stock_report(inventory: dict) -> str:
+    pass
+
