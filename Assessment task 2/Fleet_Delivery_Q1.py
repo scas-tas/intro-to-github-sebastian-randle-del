c77c2c0226229a61a841a833d793d47481cb5aed
sebastian-randle-del <sebastian-randle@scas.nsw.edu.au>
2026-07-20 12:06:01 +1000

    Qestion1 completed

--- Assessment task 2/Fleet_Delivery_Q1.py
+++ Assessment task 2/Fleet_Delivery_Q1.py
@@ -1,14 +1,17 @@
 # Fleet Delivery Q1
 def combine_trucks(trucks, first_truck, second_truck):
-    
-    # TODO: return the total packages in first_truck and second_truck
+
+    trucks_cargo =trucks[first_truck-1] + trucks[second_truck-1] #combines the load found in bboth trucks 
+    if first_truck == second_truck:
+     print(" ERROR : same truck has been inputted twice") #demonstrates the boundary case and avoids it
+     exit()
+    print(trucks_cargo)                           # prints the combined total
+    print("first truck:", trucks[first_truck-1],", second truck:",trucks[second_truck-1])  #shows the amount of cargo in each truck
+                           # TODO: return the total packages in first_truck and second_truck
     pass
-
-
 def main():
     trucks = [4, 7, 2, 6, 9]
-    print(combine_trucks(trucks, 2, 4))  # Expected: 13
-    print(combine_trucks(trucks, 1, 3))  # Expected: 6
-
-
+    combine_trucks(trucks, 2, 4)  # Expected: 13 note: i have removed print since the code now works 
+    combine_trucks(trucks, 1, 3)  # Expected: 6
 main()
+ 
